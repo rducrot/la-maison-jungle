79c465c976bbf2cdd12a9e7154cc24f1349df06a
rducrot <ducrot.romain@gmail.com>
2023-05-24 09:38:19 +0200

    Ajout events

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,17 +1,21 @@
-import '../styles/PlantItem.css'
 import CareScale from './CareScale'
+import '../styles/PlantItem.css'
 
-function PlantItem({id, cover, name, water, light}) {
-    return (
-        <li key={id} className='lmj-plant-item'>
-            <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
-            {name}
-            <div>
-                <CareScale careType='water' scaleValue={water} />
-                <CareScale careType='light' scaleValue={light} />
-            </div>
-        </li>
-    )
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
+}
+
+function PlantItem({ cover, name, water, light }) {
+	return (
+		<li className='lmj-plant-item' onClick={() => handleClick(name)}>
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+			{name}
+			<div>
+				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='light' scaleValue={light} />
+			</div>
+		</li>
+	)
 }
 
 export default PlantItem
